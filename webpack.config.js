55cb5bb66611b80e11ca86b7849e6d0e0717af68
teobler <teobler@163.com>
2019-03-08 12:18:36 +0800

    chore: add webpack config to build .tsx to .js

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './lib/index.tsx'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'),
+    library: 'spiderUI',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  }
+};
