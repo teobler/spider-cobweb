af92d51ae5ec4516e2e5366b976479091d485d24
teobler <teobler@163.com>
2019-03-10 22:30:09 +0800

    refactor: rename Button to button

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from './Button';
+import Button from './button';
 
 ReactDOM.render(<Button/>, document.getElementById('root'));
