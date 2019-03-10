34a3e587425fffec25b57482ab6f1623fccb00d8
teobler <teobler@163.com>
2019-03-10 18:24:49 +0800

    chore: build resolve .tsx .ts .jsx .js files and exclude react library

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,9 @@ module.exports = {
   entry: {
     index: './lib/index.tsx',
   },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
   output: {
     path: path.resolve(__dirname, 'dist/lib'),
     library: 'SpiderUI',
@@ -25,4 +28,18 @@ module.exports = {
       template: 'index.html',
     })
   ],
+  externals: {
+    'react': {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
+    },
+  },
 };
