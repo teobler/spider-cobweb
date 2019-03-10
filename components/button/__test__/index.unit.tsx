fd8cd48197ecabdcd343ec510c28a796a328b08b
teobler <teobler@163.com>
2019-03-10 22:51:31 +0800

    refactor: rename directory and files

--- components/button/__test__/index.unit.tsx
+++ components/button/__test__/index.unit.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from '../button';
+import Button from '../index';
 import renderer from 'react-test-renderer';
 
 describe('button', () => {
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    index: './lib/index.tsx',
+    index: './components/index.tsx',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
   },
   output: {
-    path: path.resolve(__dirname, 'dist/lib'),
+    path: path.resolve(__dirname, 'dist/components'),
     library: 'SpiderUI',
     libraryTarget: 'umd',
   },
