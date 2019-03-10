bfb17faf9e58bedb4544241247d192027f5dc329
teobler <teobler@163.com>
2019-03-10 18:26:54 +0800

    chore: create empty button to test webpack config

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,3 +1,5 @@
-const div = document.createElement('div');
-div.innerText = 'test div';
-document.body.appendChild(div);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from './Button';
+
+ReactDOM.render(<Button/>, document.getElementById('root'));
