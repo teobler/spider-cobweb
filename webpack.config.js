094d54431d7d4b594e1d4ecc3b232fac19ac82ac
teobler <teobler@163.com>
2019-03-17 18:56:31 +0800

    chore: add sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
         test: /\.svg$/,
         loader: 'svg-sprite-loader',
       },
+      {
+        test: /\.scss$/,
+        loader: ['style-loader', 'css-loader', 'sass-loader'],
+      }
     ],
   },
 };
