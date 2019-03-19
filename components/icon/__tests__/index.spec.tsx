0b7c231b21e93b219874800add41582a7b13e3df
teobler <teobler@163.com>
2019-03-19 20:42:19 +0800

    test(icon): use enzyme and jest to test icon UI

--- components/icon/__tests__/index.spec.tsx
+++ components/icon/__tests__/index.spec.tsx
@@ -1,10 +1,18 @@
+import { mount } from 'enzyme';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Icon from '../index';
 
 describe('icon', () => {
   it('should render a empty icon', () => {
-    const emptyIconJson = renderer.create(<Icon name="wechat"/>).toJSON();
-    expect(emptyIconJson).toMatchSnapshot();
+    const iconJson = renderer.create(<Icon name="wechat"/>).toJSON();
+    expect(iconJson).toMatchSnapshot();
+  });
+
+  it('should call callback function', () => {
+    const fn = jest.fn();
+    const iconNode = mount(<div><Icon name="wechat" onClick={fn}/></div>);
+    iconNode.find('Icon').simulate('click');
+    expect(fn).toBeCalled();
   });
 });
