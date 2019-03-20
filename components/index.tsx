759199c0cf7e6bdab30d614fad562ec680b1cdee
teobler <teobler@163.com>
2019-03-20 23:07:43 +0800

    refactor: add App file to do test

--- components/index.tsx
+++ components/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Icon from './icon/index';
+import App from './App';
 
-const fn = (e: React.MouseEvent) => {console.log(e.currentTarget)};
-ReactDOM.render(<Icon name="wechat" onClick={fn} className='test-name test1'/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
