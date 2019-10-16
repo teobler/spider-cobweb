f992ceb4e2aaaae1207c34620497d8045df222ab
teobler <teobler@163.com>
2019-10-16 14:13:00 +0800

    feat(table): add basic table row component

---
+++ components/table-row/TableRow.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import combineClass from '../../utils/combineClass';
+import TableLvl2Context from '../table/TableLvl2Context';
+
+interface TableRowProps {
+  component?: keyof JSX.IntrinsicElements;
+  className?: string;
+}
+
+const TableRow: React.FunctionComponent<TableRowProps> = ({
+  component: Component = 'tr',
+  className = '',
+  ...others
+}) => {
+  return (
+    <TableLvl2Context.Provider value={{}}>
+      <Component className={combineClass('table-row', className)} {...others} />
+    </TableLvl2Context.Provider>
+  );
+};
+
+export default TableRow;
