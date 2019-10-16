9aba1d2f11720743c66f1f868d6409d64067469b
teobler <teobler@163.com>
2019-10-16 16:46:33 +0800

    chore(admin): fix router issue

--- admin/App.tsx
+++ admin/App.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import { HashRouter as Router, Link, Route } from 'react-router-dom';
+import { Link, Route, useLocation } from 'react-router-dom';
 import Aside from '../components/layout/aside';
 import Content from '../components/layout/content';
 import Header from '../components/layout/header';
@@ -16,54 +16,51 @@ import LayoutDemo from './example/layout.demo';
 import MenuDemo from './example/menu.demo';
 import TableDemo from './example/table.demo';
 
-const App: React.FunctionComponent = (): ReactElement => {
-  return (
-    <Router>
-      <Layout>
-        <Header className="header">
-          <a href="#" className="logo-link">
-            <img src="../resources/logo.png" alt="logo" className="logo" />
-          </a>
-        </Header>
-        <Layout className="main">
-          <Aside className="aside">
-            <Menu defaultSelectedKey="icon">
-              <SubMenu title="Components" uniqueKey="submenu" openPopup={true}>
-                <SubList>
-                  <ListItem uniqueKey="icon">
-                    <Link to="/icon">icon</Link>
-                  </ListItem>
-                  <ListItem uniqueKey="button">
-                    <Link to="/button">button</Link>
-                  </ListItem>
-                  <ListItem uniqueKey="layout">
-                    <Link to="/layout">layout</Link>
-                  </ListItem>
-                  <ListItem uniqueKey="grid">
-                    <Link to="/grid">grid</Link>
-                  </ListItem>
-                  <ListItem uniqueKey="menu">
-                    <Link to="/menu">menu</Link>
-                  </ListItem>
-                  <ListItem uniqueKey="table">
-                    <Link to="/table">table</Link>
-                  </ListItem>
-                </SubList>
-              </SubMenu>
-            </Menu>
-          </Aside>
-          <Content className="content">
-            <Route path="/icon" component={IconDemo} />
-            <Route path="/button" component={ButtonDemo} />
-            <Route path="/layout" component={LayoutDemo} />
-            <Route path="/grid" component={GridDemo} />
-            <Route path="/menu" component={MenuDemo} />
-            <Route path="/table" component={TableDemo} />
-          </Content>
-        </Layout>
-      </Layout>
-    </Router>
-  );
-};
+const App: React.FunctionComponent = (): ReactElement => (
+  <Layout>
+    <Header className="header">
+      <a href="#" className="logo-link">
+        <img src="../resources/logo.png" alt="logo" className="logo" />
+      </a>
+    </Header>
+    <Layout className="main">
+      <Aside className="aside">
+        <Menu defaultSelectedKey={useLocation().pathname.replace('/', '')}>
+          <SubMenu title="Components" uniqueKey="submenu" openPopup={true}>
+            <SubList>
+              <ListItem uniqueKey="icon">
+                <Link to="/icon">icon</Link>
+              </ListItem>
+              <ListItem uniqueKey="button">
+                <Link to="/button">button</Link>
+              </ListItem>
+              <ListItem uniqueKey="layout">
+                <Link to="/layout">layout</Link>
+              </ListItem>
+              <ListItem uniqueKey="grid">
+                <Link to="/grid">grid</Link>
+              </ListItem>
+              <ListItem uniqueKey="menu">
+                <Link to="/menu">menu</Link>
+              </ListItem>
+              <ListItem uniqueKey="table">
+                <Link to="/table">table</Link>
+              </ListItem>
+            </SubList>
+          </SubMenu>
+        </Menu>
+      </Aside>
+      <Content className="content">
+        <Route path="/" />
+        <Route path="/icon" component={IconDemo} />
+        <Route path="/button" component={ButtonDemo} />
+        <Route path="/layout" component={LayoutDemo} />
+        <Route path="/grid" component={GridDemo} />
+        <Route path="/menu" component={MenuDemo} />
+        <Route path="/table" component={TableDemo} />
+      </Content>
+    </Layout>
+  </Layout>
+);
 
 export default App;
--- admin/index.tsx
+++ admin/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { HashRouter as Router } from 'react-router-dom';
 import '../components/style/base.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root'),
+);
