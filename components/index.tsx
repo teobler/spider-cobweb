8d2a607702ce9b1b48cc4bc3a8cd56c549002a6d
teobler <teobler@163.com>
2019-10-16 14:14:33 +0800

    refactor(table): rename index file from tsx to ts and add table export to root index file

--- components/index.tsx
+++ components/index.tsx
@@ -2,3 +2,5 @@ export { default as Icon } from './icon';
 export { default as Table } from './table';
 export { default as TableHead } from './table-head';
 export { default as TableBody } from './table-body';
+export { default as TableFooter } from './table-footer';
+export { default as TableRow } from './table-row';
