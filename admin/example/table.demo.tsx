715966ec609dc9c79a6f9008a51c3b89b2923631
teobler <teobler@163.com>
2019-10-16 11:20:11 +0800

    feat(table): add basic table footer component

--- admin/example/table.demo.tsx
+++ admin/example/table.demo.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { Table, TableBody, TableHead } from '../../components';
+import TableFooter from '../../components/table-footer/TableFooter';
 
 const TableDemo: React.FunctionComponent = () => {
   return (
     <Table>
-      <TableHead>test</TableHead>
-      <TableBody>test</TableBody>
+      <TableHead>head</TableHead>
+      <TableBody>body</TableBody>
+      <TableFooter>footer</TableFooter>
     </Table>
   );
 };
---
+++ components/table-footer/TableFooter.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import combineClass from '../../utils/combineClass';
+import TableLvl2Context from '../table/TableLvl2Context';
+
+interface TableFooterProps {
+  component?: keyof JSX.IntrinsicElements;
+  className?: string;
+}
+
+const TableFooter: React.FunctionComponent<TableFooterProps> = ({
+  component: Component = 'tfoot',
+  className = '',
+  ...others
+}) => {
+  return (
+    <TableLvl2Context.Provider value={{}}>
+      <Component className={combineClass('table-footer', className)} {...others} />
+    </TableLvl2Context.Provider>
+  );
+};
+
+export default TableFooter;
