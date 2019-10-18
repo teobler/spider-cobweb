6b59afa7a79b91c70a49a662481cd9c64b364782
teobler <teobler@163.com>
2019-10-18 15:45:24 +0800

    test(table): update table component snapshot

--- components/table/__tests__/table.spec.tsx
+++ components/table/__tests__/table.spec.tsx
@@ -1,10 +1,30 @@
 import * as React from 'react';
-import { Table } from '../../index';
+import { Table, TableBody, TableCell, TableFooter, TableHead, TableRow } from '../../index';
 import renderer from 'react-test-renderer';
 
 describe('<Table />', () => {
   it('should render a empty table', () => {
-    const menuJson = renderer.create(<Table />).toJSON();
+    const menuJson = renderer
+      .create(
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell />
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell />
+            </TableRow>
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell />
+            </TableRow>
+          </TableFooter>
+        </Table>,
+      )
+      .toJSON();
     expect(menuJson).toMatchSnapshot();
   });
 });
